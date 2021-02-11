0229902cbc1e9929c0a6262ebd2422e7cfa1ddca
hal3e <hal3e.git@gmail.com>
2021-02-11 10:32:24 +0100

    Update fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -24,11 +24,12 @@ function accept-suggest-and-exec
    commandline -f execute 
 end
 
-set -x SKIM_DEFAULT_OPTIONS '--bind=ctrl-e:up --color=bg+:#3B4252,spinner:#81A1C1,hl:#616E88,fg:#D8DEE9,header:#616E88,info:#81A1C1,pointer:#81A1C1,marker:#81A1C1,fg+:#D8DEE9,prompt:#81A1C1,hl+:#81A1C1'
+set -x SKIM_DEFAULT_OPTIONS '--bind=ctrl-e:up --color=bg+:#47516b,spinner:#81A1C1,hl:#47516b,fg:#D8DEE9,header:#47516b,info:#81A1C1,pointer:#81A1C1,marker:#81A1C1,fg+:#D8DEE9,prompt:#81A1C1,hl+:#81A1C1'
 
 set -x EDITOR nvim
+set -x SUDO_ASKPASS /home/hal3e/scpt/askpass/pass-term.sh
 
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-eval /home/hal3e/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+eval /home/hal3e/bin/miniconda3/bin/conda "shell.fish" "hook" $argv | source
 # <<< conda initialize <<<
---
+++ fish/.config/fish/functions/brightness.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 1
+function brightness --wraps='~/scrpt/brightness.sh' --wraps='~/scpt/brightness.sh' --description 'alias brightness=~/scpt/brightness.sh'
+  ~/scpt/brightness.sh  $argv;
+end

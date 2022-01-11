8ded56124f71f006e07eb0220f31f931353d8a6d
hal3e <hal3e.git@gmail.com>
2022-01-11 19:39:32 +0100

    feat: update skim colorcheme and remove scp alias

--- fish/.config/fish/functions/scp.fish
+++
@@ -1,7 +0,0 @@
-# Defined via `source`
-function scp
-  set -x SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
-  set -x GPG_TTY (tty);
-  gpg-connect-agent UPDATESTARTUPTTY /bye > /dev/null;
-  /usr/bin/scp $argv; 
-end

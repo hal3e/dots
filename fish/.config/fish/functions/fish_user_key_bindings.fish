6410b4e2360fcfc57b1d7f686cefaedba5d57f7b
hal3e <hal3e.git@gmail.com>
2023-04-26 18:41:46 +0200

    feat(fish): remove workman vi kb

--- fish/.config/fish/functions/fish_user_key_bindings.fish
+++ fish/.config/fish/functions/fish_user_key_bindings.fish
@@ -5,5 +5,6 @@ function fish_user_key_bindings
   bind -M insert \ca accept-suggest-and-exec
   bind -M insert \cr accept-autosuggestion
 
-  fish_vi_workman_key_bindings
+  # Add a way to switch from insert to normal (command) mode.
+  bind -s --preset -M insert nn "if commandline -P; commandline -f cancel; else; set fish_bind_mode default; commandline -f backward-char repaint-mode; end"
 end
